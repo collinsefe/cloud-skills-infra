5e849965858ff678059313f91cd0d34d5f9c01a8
legado22 <lesmarkoladipupo402@mail.com>
2024-12-07 23:08:21 +0100

    new changes

--- Downloads/Ec2 with Terraform/teni-ec2.tf
+++ Downloads/Ec2 with Terraform/teni-ec2.tf
@@ -3,11 +3,12 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-resource "aws_instance" "Teni" {
+
+resource "aws_instance" "teni" {
   ami           = "ami-02f617729751b375a"
   instance_type = "t2.micro"
   key_name      = "tennie key"
   tags = {
-    Name = "Teni-Instance"
+    Name = "tennie-instance"
   }
 }

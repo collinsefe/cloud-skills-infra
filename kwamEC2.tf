fc823c41a31b6cb666242672b946ed4db8339218
Kwamdeen <ayinlakwamdeen@yahoo.com>
2024-12-07 21:57:59 +0000

    updated document paths 22

--- kwamEC2.tf
+++ kwamEC2.tf
@@ -1,10 +1,10 @@
-#resource "aws_instance" "kwam_2" {
-# ami           = "ami-0c76bd4bd302b30ec"
-#instance_type = "t2.micro"
+resource "aws_instance" "kwam_2" {
+ ami           = "ami-0c76bd4bd302b30ec"
+instance_type = "t2.micro"
 
-#key_name = "kwam-key"
+key_name = "kwam-key"
 
-#tags = {
-#  Name = "kwam_2"
-#}
-#}
+tags = {
+  Name = "kwam_2"
+}
+}

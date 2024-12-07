13e5bab8f90127ae41b9b61ce84fe3181aded522
legado22 <lesmarkoladipupo402@mail.com>
2024-12-07 11:11:12 +0100

    Ec2 with Terraform

--- Downloads/Ec2 with Terraform/teni-ec2.tf
+++ Downloads/Ec2 with Terraform/teni-ec2.tf
@@ -1,8 +1,13 @@
-resource "aws_instance" "Shiloh-2024" {
-  ami           = "ami-02f617729751b375a" #
-  instance_type = "t2.micro"
+provider "aws" {
+  profile = "692859905059_AWSPowerUserAccess"
+  region = "eu-west-2"
+}
 
+resource "aws_instance" "Teni" {
+  ami           = "ami-02f617729751b375a"
+  instance_type = "t2.micro"
+  key_name      = "tennie key"
   tags = {
-    Name : "Teni Instance"
+    Name = "Teni-Instance"
   }
 }

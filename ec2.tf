2e3196f823200a0c164f6e53b5271990add190c1
Collins Orighose <collinsefe@yahoo.com>
2024-12-08 21:44:34 +0000

    restore resources

--- ec2.tf
+++ ec2.tf
@@ -5,3 +5,38 @@ resource "aws_instance" "this" {
     Name : "Project Instance DO-NOT-DELETE"
   }
 }
+
+resource "aws_instance" "gbos" {
+  ami           = "ami-02f617729751b375a"
+  instance_type = "t2.micro"
+  tags = {
+    Name : "Bimbo Instance"
+  }
+}
+
+resource "aws_instance" "bar" {
+  ami           = "ami-02f617729751b375a"
+  instance_type = "t2.micro"
+  tags = {
+    Name : "Collins Instance"
+  }
+}
+
+resource "aws_instance" "kwam_2" {
+  ami           = "ami-0c76bd4bd302b30ec"
+  instance_type = "t2.micro"
+  key_name      = "kwam-key"
+  tags = {
+    Name = "Kwam instance"
+  }
+}
+
+resource "aws_instance" "tennie" {
+  ami           = "ami-02f617729751b375a"
+  instance_type = "t2.micro"
+  key_name      = "tennie key"
+  tags = {
+    Name = "tennie-instance"
+  }
+}
+

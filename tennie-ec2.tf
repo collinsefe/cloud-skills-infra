d2464d404ef0df62bd3856fa9e7d402338e7c651
Collins Orighose <collinsefe@yahoo.com>
2024-12-08 21:07:30 +0000

    CLOUD-15-refactor-code

--- tennie-ec2.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "tennie" {
-  ami           = "ami-02f617729751b375a"
-  instance_type = "t2.micro"
-  key_name      = "tennie key"
-  tags = {
-    Name = "tennie-instance"
-  } 
-}

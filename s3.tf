0b61e42a582b3f9a5b029d3bc036c01977bb3a22
Collins Orighose <collinsefe@yahoo.com>
2024-12-08 21:43:18 +0000

    create automation for terraform

--- s3.tf
+++ s3.tf
@@ -1,35 +1,8 @@
-resource "aws_s3_bucket" "drink" {
-  bucket = "kwam-terraform-s3bucket"
+resource "aws_s3_bucket" "this" {
+  bucket = "cloudskills-project-s3-bucket"
 
   tags = {
-    Name        = "Kwam bucket"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_s3_bucket" "food" {
-  bucket = "collinsefe-terraform-s3bucket-09087654q"
-
-  tags = {
-    Name        = "Collins bucket"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_s3_bucket" "coke" {
-  bucket = "tennie-terraform-s3bucket-09087654q"
-
-  tags = {
-    Name        = "Tennie bucket"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_s3_bucket" "fanta" {
-  bucket = "bimbo-terraform-s3bucket-09087654q"
-
-  tags = {
-    Name        = "Bimbo bucket"
+    Name        = "cloudskills bucket"
     Environment = "Dev"
   }
 }

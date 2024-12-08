12e10495f2a63029e5ef64b3074a5f9b4c4f8322
aaabimbola <aaabimbola@gmail.com>
2024-12-08 20:19:18 +0000

    bimbo ec2

--- bimbo14-es2.tf
+++ bimbo14-es2.tf
@@ -1,2 +1,11 @@
 
-abimbola ec2 instance using terraform
+
+resource "aws_instance" "bar" {
+    ami           = "ami-02f617729751b375a" 
+      instance_type = "t2.micro"
+
+        tags = {
+              Name : "Bimbo Instance"
+                }
+              }
+/bin/bash: line 1: qw:: command not found

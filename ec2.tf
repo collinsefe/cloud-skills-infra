e7a14906dcce0cd45449d1c8263b80c7893d4513
Collins Orighose <collinsefe@yahoo.com>
2024-12-09 18:28:42 +0000

    refactor-code

--- ec2.tf
+++ ec2.tf
@@ -6,37 +6,3 @@ resource "aws_instance" "this" {
   }
 }
 
-resource "aws_instance" "gbos" {
-  ami           = "ami-02f617729751b375a"
-  instance_type = "t2.micro"
-  tags = {
-    Name : "Bimbo Instance"
-  }
-}
-
-resource "aws_instance" "bar" {
-  ami           = "ami-02f617729751b375a"
-  instance_type = "t2.micro"
-  tags = {
-    Name : "Collins Instance"
-  }
-}
-
-resource "aws_instance" "kwam_2" {
-  ami           = "ami-0c76bd4bd302b30ec"
-  instance_type = "t2.micro"
-  key_name      = "kwam-key"
-  tags = {
-    Name = "Kwam instance"
-  }
-}
-
-resource "aws_instance" "tennie" {
-  ami           = "ami-02f617729751b375a"
-  instance_type = "t2.micro"
-  key_name      = "tennie key"
-  tags = {
-    Name = "tennie-instance"
-  }
-}
-

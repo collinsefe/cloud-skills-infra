e40d083966a2daa2c140847a4e5994052cf1ddc4
Collins Orighose <collinsefe@yahoo.com>
2024-12-14 21:52:08 +0000

    jenkins demo changes

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "drink" {
-  bucket = "cloudskills-terraform-s3-bucket"
+  bucket = "cloudskills-terraform-s3-bucket-2"
 
   tags = {
     Name        = "Cloudskills bucket"

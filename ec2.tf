de5562b630dfff4dda6bcc70b6ec99e0481a41ef
legado22 <lesmarkoladipupo402@mail.com>
2024-12-14 18:03:24 +0100

    New block added to main

--- ec2.tf
+++ ec2.tf
@@ -5,4 +5,10 @@ resource "aws_instance" "this" {
     Name : "Project Instance DO-NOT-DELETE"
   }
 }
-
+resource "aws_instance" "tennie" {
+  ami           = "ami-02f617729751b375a"
+  instance_type = "t2.micro"
+  tags = {
+    Name : "tennie-instance"
+ }
+}

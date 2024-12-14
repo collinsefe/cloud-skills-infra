d301d65a9b1be77ee547ba1b142a65f4f7938673
Kwamdeen <ayinlakwamdeen@yahoo.com>
2024-12-14 12:52:21 +0000

    CLOUD-18/added a new EC2 block to ec2.tf file

--- ec2.tf
+++ ec2.tf
@@ -14,3 +14,11 @@ resource "aws_instance" "bar" {
     Name : "Collins Instance"
   }
 }
+
+resource "aws_instance" "kwamdeen" {
+  ami           = "ami-02f617729751b375a"
+  instance_type = "t2.micro"
+  tags = {
+    Name : "Kwamdeen Ayinla"
+  }
+}

9dcb0bd18ac51eddf4920817fbb5fb45abc4a0d8
Collins Orighose <collinsefe@yahoo.com>
2024-12-15 18:53:40 +0000

    updated ec2

--- ec2.tf
+++ ec2.tf
@@ -6,14 +6,14 @@ resource "aws_instance" "this" {
   }
 }
 
-# resource "aws_instance" "bar" {
-#   ami           = "ami-02f617729751b375a"
-#   instance_type = "t2.micro"
+resource "aws_instance" "bar" {
+  ami           = "ami-02f617729751b375a"
+  instance_type = "t2.micro"
 
-#   tags = {
-#     Name : "Collins Instance"
-#   }
-# }
+  tags = {
+    Name : "Collins Instance"
+  }
+}
 
 # resource "aws_instance" "kwamdeen" {
 #   ami           = "ami-02f617729751b375a"

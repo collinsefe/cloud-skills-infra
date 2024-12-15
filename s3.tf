8e62c307761a80daa80d624e63345470decde440
Collins Orighose <collinsefe@yahoo.com>
2024-12-15 21:24:28 +0000

    create ec2 and s3 for dev1 and dev1

--- s3.tf
+++ s3.tf
@@ -7,30 +7,23 @@ resource "aws_s3_bucket" "drink" {
   }
 }
 
-# resource "aws_s3_bucket" "teni" {
-#   bucket = "tenny-terraform-s3bucket"
-#   tags = {
-#     Name        = "tenny bucket"
-#      Environment = "Dev"
-#   }
-# }
+resource "aws_s3_bucket" "dev-1" {
+  bucket = "cloudskills-developer-1-s3-bucket"
+
+  tags = {
+    Name        = "Developer 1 bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "dev-2" {
+  bucket = "cloudskills-developer-2-s3-bucket"
+
+  tags = {
+    Name        = "Developer 2 bucket"
+    Environment = "Dev"
+  }
+}
 
 
-# resource "aws_s3_bucket" "kwamdeen" {
-#   bucket = "kwamdeen-terraform-s3bucket"
 
-#   tags = {
-#     Name        = "Kwamdeen Ayinla bucket"
-#     Environment = "Dev"
-#   }
-# }
-
-
-# resource "aws_s3_bucket" "example" {
-#   bucket        = "teni-s3-bucket" 
-
-#   tags = {
-#     Name        = "MyBucket"
-#     Environment = "Dev"
-#   }
-# }

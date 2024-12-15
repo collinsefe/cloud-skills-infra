9e0b7e25f152ba977213af3944540d34f3baa8f7
Collins Orighose <collinsefe@yahoo.com>
2024-12-15 18:47:49 +0000

    updated ec2 and s3

--- local.tf
+++ local.tf
@@ -1,10 +1,10 @@
-resource "local_file" "this" {
-  content  = "My Name is Jonathan"
-  filename = "${path.module}/john.txt"
-}
+# resource "local_file" "this" {
+#   content  = "My Name is Jonathan"
+#   filename = "${path.module}/john.txt"
+# }
 
 
-resource "local_file" "just" {
-  content  = "My Name is Catherine"
-  filename = "${path.module}/cat.txt"
-}
+# resource "local_file" "just" {
+#   content  = "My Name is Catherine"
+#   filename = "${path.module}/cat.txt"
+# }

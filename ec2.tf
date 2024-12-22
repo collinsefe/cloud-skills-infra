b4224a6e90ecbaad56081db3516965cb940537fc
Collins Orighose <collinsefe@yahoo.com>
2024-12-22 19:57:31 +0000

    update infra

--- ec2.tf
+++ ec2.tf
@@ -1,59 +1,59 @@
-# This file contains the configuration for creating EC2 instances
+# # This file contains the configuration for creating EC2 instances
 
-resource "aws_instance" "this" {
-  ami           = "ami-02f617729751b375a"
-  instance_type = "t2.micro"
-  tags = {
-    Name : "Project Instance DO-NOT-DELETE"
-  }
-}
-
-# resource "aws_instance" "dev-1" {
+# resource "aws_instance" "this" {
 #   ami           = "ami-02f617729751b375a"
 #   instance_type = "t2.micro"
 #   tags = {
-#     Name : "Developer 1"
+#     Name : "Project Instance DO-NOT-DELETE"
 #   }
 # }
 
-# resource "aws_instance" "dev-2" {
-#   ami           = "ami-02f617729751b375a"
-#   instance_type = "t2.micro"
-#   tags = {
-#     Name : "Developer 2"
-#   }
-# }
+# # resource "aws_instance" "dev-1" {
+# #   ami           = "ami-02f617729751b375a"
+# #   instance_type = "t2.micro"
+# #   tags = {
+# #     Name : "Developer 1"
+# #   }
+# # }
 
-# resource "aws_instance" "fish" {
-#   ami           = "ami-02f617729751b375a"
-#   instance_type = "t2.micro"
-#   tags = {
-#     Name : "Collins Instance"
-#   }
-# }
+# # resource "aws_instance" "dev-2" {
+# #   ami           = "ami-02f617729751b375a"
+# #   instance_type = "t2.micro"
+# #   tags = {
+# #     Name : "Developer 2"
+# #   }
+# # }
 
-# resource "aws_instance" "kwamdeen" {
-#   ami           = "ami-02f617729751b375a"
-#   instance_type = "t2.micro"
-#   tags = {
-#     Name = "Developer kwamdeen"
-#   }
-# }
+# # resource "aws_instance" "fish" {
+# #   ami           = "ami-02f617729751b375a"
+# #   instance_type = "t2.micro"
+# #   tags = {
+# #     Name : "Collins Instance"
+# #   }
+# # }
 
-# resource "aws_instance" "tenny" {
-#   ami           = "ami-02f617729751b375a"
-#   instance_type = "t2.micro"
-#   tags = {
-#     Name = "Developer tennie"
-#   }
-# }
+# # resource "aws_instance" "kwamdeen" {
+# #   ami           = "ami-02f617729751b375a"
+# #   instance_type = "t2.micro"
+# #   tags = {
+# #     Name = "Developer kwamdeen"
+# #   }
+# # }
+
+# # resource "aws_instance" "tenny" {
+# #   ami           = "ami-02f617729751b375a"
+# #   instance_type = "t2.micro"
+# #   tags = {
+# #     Name = "Developer tennie"
+# #   }
+# # }
 
 
-# resource "aws_instance" "that" {
-#   ami           = "ami-02f617729751b375a"
-#   instance_type = "t2.micro"
-#   tags = {
-#     Name : "Bimbo EC2"
-#   }
-# }
+# # resource "aws_instance" "that" {
+# #   ami           = "ami-02f617729751b375a"
+# #   instance_type = "t2.micro"
+# #   tags = {
+# #     Name : "Bimbo EC2"
+# #   }
+# # }
 
--- s3.tf
+++ s3.tf
@@ -1,57 +1,57 @@
-# This file contains the configuration for creating EC2 instances
+# # This file contains the configuration for creating EC2 instances
 
-resource "aws_s3_bucket" "foo" {
-  bucket = "cloudskills-team-terraform-s3-bucket"
-
-  tags = {
-    Name        = "Cloudskills bucket"
-    Environment = "Dev"
-  }
-}
-
-# resource "aws_s3_bucket" "dev-1" {
-#   bucket = "cloudskills-developer-1-s3-bucket"
+# resource "aws_s3_bucket" "foo" {
+#   bucket = "cloudskills-team-terraform-s3-bucket"
 
 #   tags = {
-#     Name        = "Developer 1 bucket"
+#     Name        = "Cloudskills bucket"
 #     Environment = "Dev"
 #   }
 # }
 
-# resource "aws_s3_bucket" "dev-2" {
-#   bucket = "cloudskills-developer-2-s3-bucket"
+# # resource "aws_s3_bucket" "dev-1" {
+# #   bucket = "cloudskills-developer-1-s3-bucket"
 
-#   tags = {
-#     Name        = "Developer 2 bucket"
-#     Environment = "Dev"
-#   }
-# }
+# #   tags = {
+# #     Name        = "Developer 1 bucket"
+# #     Environment = "Dev"
+# #   }
+# # }
 
-# resource "aws_s3_bucket" "kwamdeen" {
-#   bucket = "cloudskills-developer-2-s3-bucket-kwamdeen"
+# # resource "aws_s3_bucket" "dev-2" {
+# #   bucket = "cloudskills-developer-2-s3-bucket"
 
-#   tags = {
-#     Name        = "Developer kwamdeen bucket"
-#     Environment = "Dev"
-#   }
-# }
+# #   tags = {
+# #     Name        = "Developer 2 bucket"
+# #     Environment = "Dev"
+# #   }
+# # }
+
+# # resource "aws_s3_bucket" "kwamdeen" {
+# #   bucket = "cloudskills-developer-2-s3-bucket-kwamdeen"
+
+# #   tags = {
+# #     Name        = "Developer kwamdeen bucket"
+# #     Environment = "Dev"
+# #   }
+# # }
 
 
-# resource "aws_s3_bucket" "fish" {
-#   bucket = "cloudskills-collins-2-s3-bucket"
+# # resource "aws_s3_bucket" "fish" {
+# #   bucket = "cloudskills-collins-2-s3-bucket"
 
-#   tags = {
-#     Name        = "Collins bucket"
-#     Environment = "Dev"
-#   }
-# }
+# #   tags = {
+# #     Name        = "Collins bucket"
+# #     Environment = "Dev"
+# #   }
+# # }
 
 
-# resource "aws_s3_bucket" "Bimbo" {
-#   bucket = "cloudskills-bimbo-s3-bucket"
+# # resource "aws_s3_bucket" "Bimbo" {
+# #   bucket = "cloudskills-bimbo-s3-bucket"
 
-#   tags = {
-#     Name        = "Developer bimbo bucket"
-#     Environment = "Dev"
-#   }
-# }
+# #   tags = {
+# #     Name        = "Developer bimbo bucket"
+# #     Environment = "Dev"
+# #   }
+# # }

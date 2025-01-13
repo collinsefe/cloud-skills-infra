ace97528bba9e3ea8662d90bc46f7f2570a8b82e
Collins Orighose <collinsefe@yahoo.com>
2025-01-13 15:59:20 +0000

    updated ansible files

--- ec2.tf
+++ ec2.tf
@@ -1,31 +1,31 @@
-# # # This file contains the configuration for creating EC2 instances
+# # # # This file contains the configuration for creating EC2 instances
 
 resource "aws_instance" "ansible" {
-  ami                         = "ami-02f617729751b375a"
+  ami                         = "ami-0a89c0e1fe86ef74e"
   instance_type               = "t2.micro"
   user_data                   = file("tools/ansible_install.sh")
   associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.web.id]
+  vpc_security_group_ids      = [aws_security_group.fix.id]
   tags = {
     Name : "ansible-control-node"
   }
 }
 
 resource "aws_instance" "java" {
-  ami                         = "ami-02f617729751b375a"
+  ami                         = "ami-0a89c0e1fe86ef74e"
   instance_type               = "t2.micro"
   associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.web.id]
+  vpc_security_group_ids      = [aws_security_group.fix.id]
   tags = {
     Name : "app-server"
   }
 }
 
 resource "aws_instance" "redis" {
-  ami                         = "ami-02f617729751b375a"
+  ami                         = "ami-0a89c0e1fe86ef74e"
   instance_type               = "t2.micro"
   associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.web.id]
+  vpc_security_group_ids      = [aws_security_group.fix.id]
   tags = {
     Name : "db-server"
   }
@@ -33,10 +33,10 @@ resource "aws_instance" "redis" {
 
 
 resource "aws_instance" "nginx" {
-  ami                         = "ami-02f617729751b375a"
+  ami                         = "ami-0a89c0e1fe86ef74e"
   instance_type               = "t2.micro"
   associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.web.id]
+  vpc_security_group_ids      = [aws_security_group.fix.id]
   tags = {
     Name : "web-server"
   }
--- vpc.tf
+++ vpc.tf
@@ -1,20 +1,18 @@
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+  cidr_block = "172.16.0.0/16"
 
   tags = {
-    Name = "CloudSkills VPC"
+    Name = "cloudSkills-vpc"
   }
 }
 
-
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.10.0/24"
+  cidr_block        = "172.16.10.0/24"
   availability_zone = "eu-west-2a"
 
   tags = {
-    Name = "CloudSkills Subnet"
+    Name = "cloudSkills-subnet-2a"
   }
 }
 

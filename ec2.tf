8dc1b4efeb3ef4579a3ea7926b75700fec762f64
Collins Orighose <collinsefe@yahoo.com>
2025-01-13 11:23:57 +0000

    updated ansible files

--- ec2.tf
+++ ec2.tf
@@ -8,32 +8,40 @@ resource "aws_instance" "ansible" {
   vpc_security_group_ids      = [aws_security_group.web.id]
 
   tags = {
-    Name : "ansible node"
+    Name : "ansible-control-node"
   }
 }
 
-# resource "aws_instance" "java" {
-#   ami           = "ami-02f617729751b375a"
-#   instance_type = "t2.micro"
-#   tags = {
-#     Name : "app server"
-#   }
-# }
+resource "aws_instance" "java" {
+  ami                         = "ami-02f617729751b375a"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.web.id]
 
-# resource "aws_instance" "redis" {
-#   ami           = "ami-02f617729751b375a"
-#   instance_type = "t2.micro"
-#   tags = {
-#     Name : "db server"
-#   }
-# }
+  tags = {
+    Name : "app-server"
+  }
+}
+
+resource "aws_instance" "redis" {
+  ami                         = "ami-02f617729751b375a"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.web.id]
+
+  tags = {
+    Name : "db-server"
+  }
+}
 
 
-# resource "aws_instance" "nginx" {
-#   ami           = "ami-02f617729751b375a"
-#   instance_type = "t2.micro"
-#   tags = {
-#     Name : "web server"
-#   }
-# }
+resource "aws_instance" "nginx" {
+  ami                         = "ami-02f617729751b375a"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.web.id]
+  tags = {
+    Name : "web-server"
+  }
+}
 

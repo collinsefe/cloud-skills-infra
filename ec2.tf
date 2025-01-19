da7875b0b5984f9ec479bf484782504bdc07567d
Collins Orighose <collinsefe@yahoo.com>
2025-01-19 22:00:09 +0000

    update ec2 instances

--- ec2.tf
+++ ec2.tf
@@ -1,35 +1,35 @@
-# # # # This file contains the configuration for creating EC2 instances
+# # # # # This file contains the configuration for creating EC2 instances
 
-resource "aws_instance" "ansible" {
+# resource "aws_instance" "ansible" {
+#   ami                         = "ami-0a89c0e1fe86ef74e"
+#   instance_type               = "t2.micro"
+#   user_data                   = file("tools/ansible_install.sh")
+#   associate_public_ip_address = true
+#   vpc_security_group_ids      = [aws_security_group.web.id]
+#   key_name                    = "ansible"
+#   provisioner "file" {
+#     source      = "ansible-configuration"
+#     destination = "ansible"
+#   }
+
+#   connection {
+#     type        = "ssh"
+#     user        = "ec2-user"
+#     private_key = aws_key_pair.this.id
+#     host        = aws_instance.ansible.public_ip
+#     port        = 22
+#   }
+#   tags = {
+#     Name : "ansible-control-node"
+#   }
+# }
+
+resource "aws_instance" "this" {
   ami                         = "ami-0a89c0e1fe86ef74e"
   instance_type               = "t2.micro"
-  user_data                   = file("tools/ansible_install.sh")
-  associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.web.id]
-  key_name                    = "ansible"
-  provisioner "file" {
-    source      = "ansible-configuration"
-    destination = "ansible"
-  }
-
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    private_key = aws_key_pair.this.id
-    host        = aws_instance.ansible.public_ip
-    port        = 22
-  }
-  tags = {
-    Name : "ansible-control-node"
-  }
-}
-
-resource "aws_instance" "java" {
-  ami                         = "ami-0a89c0e1fe86ef74e"
-  instance_type               = "t2.micro"
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.web.id]
   tags = {
-    Name : "app-server"
+    Name : "collins server"
   }
 }

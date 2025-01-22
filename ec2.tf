4e123ac6eddcf80a9a429dc81b39b9fc693498f5
KWAMDEEN AYINLA <31147728+ayinla7@users.noreply.github.com>
2025-01-22 18:04:06 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -23,7 +23,6 @@
 #     Name : "ansible-control-node"
 #   }
 # }
-
 resource "aws_instance" "this" {
   ami                         = "ami-0a89c0e1fe86ef74e"
   instance_type               = "t2.micro"
@@ -39,13 +38,11 @@ resource "aws_instance" "kwam" {
   instance_type               = "t2.micro"
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.web.id]
-
   tags = {
     Name : "Kwam donker server"
   }
-
   key_name = "kwam-donker" 
-
 }
+
 
 

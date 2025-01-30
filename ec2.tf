b5e737851ab250687e4e67d0aae0a6a442279786
aaabimbola <aaabimbola@gmail.com>
2025-01-30 22:00:37 +0000

    created ec2 for docker demo

--- ec2.tf
+++ ec2.tf
@@ -33,3 +33,13 @@ resource "aws_instance" "this" {
     Name : "collins server"
   }
 }
+
+
+
+resource "aws_instance" "abimbola" {
+  ami           = "ami-02f617729751b375a"
+  instance_type = "t2.micro"
+  tags = {
+    Name : "abimbola docker demo"
+  }
+}

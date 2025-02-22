69a9b8a692bbb3caa162299c15562d680b74b70c
Collins Orighose <collinsefe@yahoo.com>
2025-02-22 21:06:54 +0000

    asg demo session

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,78 @@
 # # # # # This file contains the configuration for creating EC2 instances
 
+# resource "aws_instance" "this" {
+#   ami                         = "ami-0a89c0e1fe86ef74e"
+#   instance_type               = "t2.micro"
+#   associate_public_ip_address = true
+#   vpc_security_group_ids      = [aws_security_group.web.id]
+#   metadata_options {
+#     http_tokens = "required"
+#   }
+#   root_block_device {
+#     encrypted = true
+#   }
+
+#   ebs_block_device {
+#     device_name           = "/dev/sdg"
+#     volume_size           = 5
+#     volume_type           = "gp2"
+#     delete_on_termination = true
+#     encrypted             = true
+#   }
+
+#   tags = {
+#     Name : "collins server"
+#   }
+# }
+
+
+# resource "aws_instance" "teni" {
+#   ami                         = "ami-0a89c0e1fe86ef74e"
+#   instance_type               = "t2.micro"
+#   associate_public_ip_address = true
+#   vpc_security_group_ids      = [aws_security_group.web.id]
+#   metadata_options {
+#     http_tokens = "required"
+#   }
+#   root_block_device {
+#     encrypted = true
+#   }
+#   ebs_block_device {
+#     device_name           = "/dev/sdg"
+#     volume_size           = 5
+#     volume_type           = "gp2"
+#     delete_on_termination = true
+#     encrypted             = true
+#   }
+#   tags = {
+#     Name : "tennie server"
+#   }
+# }
+
+# resource "aws_instance" "abimbola" {
+#   ami                         = "ami-02f617729751b375a"
+#   instance_type               = "t2.micro"
+#   associate_public_ip_address = true
+#   vpc_security_group_ids      = [aws_security_group.web.id]
+#   metadata_options {
+#     http_tokens = "required"
+#   }
+#   root_block_device {
+#     encrypted = true
+#   }
+#   ebs_block_device {
+#     device_name           = "/dev/sdg"
+#     volume_size           = 5
+#     volume_type           = "gp2"
+#     delete_on_termination = true
+#     encrypted             = true
+#   }
+#   tags = {
+#     Name : "abimbola docker demo"
+#   }
+# }
+
+
 # resource "aws_instance" "ansible" {
 #   ami                         = "ami-0a89c0e1fe86ef74e"
 #   instance_type               = "t2.micro"
@@ -24,74 +97,3 @@
 #   }
 # }
 
-resource "aws_instance" "this" {
-  ami                         = "ami-0a89c0e1fe86ef74e"
-  instance_type               = "t2.micro"
-  associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.web.id]
-  metadata_options {
-    http_tokens = "required"
-  }
-  root_block_device {
-    encrypted = true
-  }
-
-  ebs_block_device {
-    device_name           = "/dev/sdg"
-    volume_size           = 5
-    volume_type           = "gp2"
-    delete_on_termination = true
-    encrypted             = true
-  }
-
-  tags = {
-    Name : "collins server"
-  }
-}
-
-
-resource "aws_instance" "teni" {
-  ami                         = "ami-0a89c0e1fe86ef74e"
-  instance_type               = "t2.micro"
-  associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.web.id]
-  metadata_options {
-    http_tokens = "required"
-  }
-  root_block_device {
-    encrypted = true
-  }
-  ebs_block_device {
-    device_name           = "/dev/sdg"
-    volume_size           = 5
-    volume_type           = "gp2"
-    delete_on_termination = true
-    encrypted             = true
-  }
-  tags = {
-    Name : "tennie server"
-  }
-}
-
-resource "aws_instance" "abimbola" {
-  ami                         = "ami-02f617729751b375a"
-  instance_type               = "t2.micro"
-  associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.web.id]
-  metadata_options {
-    http_tokens = "required"
-  }
-  root_block_device {
-    encrypted = true
-  }
-  ebs_block_device {
-    device_name           = "/dev/sdg"
-    volume_size           = 5
-    volume_type           = "gp2"
-    delete_on_termination = true
-    encrypted             = true
-  }
-  tags = {
-    Name : "abimbola docker demo"
-  }
-}

a006bec6f75f0354fdb3ad525e93bf32ec6da916
collins orighose <collinsefe@yahoo.com>
2025-06-07 19:45:35 +0100

    create 5 servers

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@
 
 resource "aws_instance" "this" {
   ami                         = "ami-0a89c0e1fe86ef74e"
+  count = "5"
   instance_type               = "t2.micro"
   associate_public_ip_address = true
   #   vpc_security_group_ids      = [aws_security_group.web.id]
